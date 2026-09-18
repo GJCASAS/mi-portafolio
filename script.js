1b58b38283a000eeb027c67d197554ec79b8a947
Gloria Casas <gloria689@hotmail.com>
2026-09-17 19:08:30 -0500

    modo oscuro

--- script.js
+++ script.js
@@ -7,4 +7,15 @@ document.addEventListener('DOMContentLoaded', () => {
             alert('¡Gracias por tu interés, Gloria! Pronto nos pondremos en contacto.');
         });
     }
+});const btnTheme = document.getElementById('btn-theme');
+
+btnTheme.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+    
+    // Cambiar el texto del botón según el modo activo
+    if (document.body.classList.contains('dark-mode')) {
+        btnTheme.textContent = '☀️ Modo Claro';
+    } else {
+        btnTheme.textContent = '🌙 Modo Oscuro';
+    }
 });
